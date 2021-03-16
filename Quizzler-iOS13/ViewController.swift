34768a39cc32dcf3d07caa5e8845d83418e68645
patelvishwa112 <patelvishwa112@gmail.com>
2021-03-15 22:41:35 -0500

    Change Label using either buttons
    Change QuestionLabel text using either buttons.

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    let quiz = ["Four + Two is equal to six",
+    "Five - Three is greater than One",
+    "Three + Eigh is less than ten"]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        questionLabel.text = quiz[0]
+        
     }
 
 
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        questionNumber+=1
+        
+        if questionNumber < 3 {
+            questionLabel.text = quiz[questionNumber]
+        }
+        else{
+            questionNumber = 0
+            questionLabel.text = quiz[questionNumber]
+        }
+        
+        
+    }
 }
 
